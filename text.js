07db11e0010369e0f47ac477646c032b53693594
David Okih <98276781+Davidokih@users.noreply.github.com>
2023-03-12 14:21:22 +0100

    Update text.js

--- text.js
+++ text.js
@@ -1,15 +1,15 @@
 
 let n = 20;
 
-// for (let i = 0; i < n; i++) {
-//     for (let j = 0; j < i; j++) {
-//         let square = i ** 2 + j ** 2;
-//         c = parseInt(Math.sqrt(square));
-//         if (square - c ** 2 == 0) {
-//             console.log(j, i, c);
-//         }
-//     }
-// }
+ for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+        let square = i ** 2 + j ** 2;
+         c = parseInt(Math.sqrt(square));
+        if (square - c ** 2 == 0) {
+             console.log(j, i, c);
+        }
+    }
+ }
 
 const myPattern = (n) => {
     let space = 2 * n - 2;
